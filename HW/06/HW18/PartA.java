54fa0680fc81b7bf8170c5347b6fd851626f88d3
tchughtai27 <27tchughtai@derryfield.org>
2024-12-16 19:00:44 -0500

    HW18
    A B C D

---
+++ HW/06/HW18/PartA.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class InClass {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("Emily");
+        names.add("Bob");
+        names.add("Cindy");
+        System.out.println(names);
+
+        ArrayList<int> values = new ArrayList<int>();
+        values.add(1);
+        values.add(42);
+    }    
+
+}
