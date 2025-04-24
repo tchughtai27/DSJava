fa1a5fe2357d6a69f85d5707f00dd254308cdbb6
tchughtai27 <27tchughtai@derryfield.org>
2025-04-24 19:22:22 -0400

    R02

--- HW/Recursion/R02/IterativePalindrome.java
+++ HW/Recursion/R02/IterativePalindrome.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
-public class Palindrome {
+public class IterativePalindrome {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a sentence to check (recursive):");
+        System.out.println("Enter a sentence to check (iterative):");
         String input = scanner.nextLine().toLowerCase().replaceAll("[^a-z]", "");
 
         if (isPalindrome(input)) {
@@ -14,12 +14,15 @@ public class Palindrome {
     }
 
     public static boolean isPalindrome(String s) {
-        if (s.length() <= 1) {
-            return true;
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
         }
-        if (s.charAt(0) != s.charAt(s.length() - 1)) {
-            return false;
-        }
-        return isPalindrome(s.substring(1, s.length() - 1));
+        return true;
     }
 }
